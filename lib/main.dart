2924dcd2112a5fc5750f1ce06b140c2f6bec31d0
Flexxbo <flexxbo@gmail.com>
2021-03-01 23:07:06 +0100

    encapsulated functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'question.dart';
+import 'quizbrain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -28,14 +30,6 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  int questionCounter = 0;
-
-  List<Question> questionBank = [
-    Question('You can lead a cow down stairs but not up stairs.', false),
-    Question('Approximately one quarter of human bones are in the feet.', true),
-    Question('A slug\'s blood is green.', true),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -48,7 +42,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionBank[questionCounter].questionText,
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -72,8 +66,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer =
-                    questionBank[questionCounter].questionAnswer;
+                bool correctAnswer = quizBrain.getQuestionAnswer();
 
                 if (correctAnswer == true) {
                   print('User picked right answer');
@@ -82,9 +75,7 @@ class _QuizPageState extends State<QuizPage> {
                 }
 
                 setState(() {
-                  if (questionCounter < questionBank.length - 1) {
-                    questionCounter++;
-                  }
+                  quizBrain.nextQuestion();
                 });
                 //The user picked true.
               },
@@ -104,8 +95,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer =
-                    questionBank[questionCounter].questionAnswer;
+                bool correctAnswer = quizBrain.getQuestionAnswer();
 
                 if (correctAnswer == false) {
                   print('User picked right answer');
@@ -114,9 +104,7 @@ class _QuizPageState extends State<QuizPage> {
                 }
 
                 setState(() {
-                  if (questionCounter < questionBank.length - 1) {
-                    questionCounter++;
-                  }
+                  quizBrain.nextQuestion();
                 });
                 //The user picked false.
               },
